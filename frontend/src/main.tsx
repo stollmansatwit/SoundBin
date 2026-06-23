19b32bba29f07125753821ae2c5560f9b59402de
stollmansatwit <stollmans@wit.edu>
2026-06-23 12:08:33 -0400

    SchemaColumns has its own page /columns

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,15 +4,16 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 import {NavBar} from './components/NavBar.tsx';
+import { SchemaColumns } from './components/SchemaColumns.tsx';
 
 const router = createBrowserRouter([
   {path: '/', element: <App />},
   {path: '/navbar', element: <NavBar />},
+  {path: '/columns', element: <SchemaColumns />},
 ]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <RouterProvider router={router} />
-
   </StrictMode>,
 )
