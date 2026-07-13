6ef17ca97fecbcb85b7aa153829e3b99d62d6eb1
stollmansatwit <stollmans@wit.edu>
2026-07-13 11:10:53 -0400

    Header redirects to home

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 export function Header() {
   return (
     <>
+    <a href="/home">
     <h1 className='flex justify-center bg-transparent shadow-lg p-4 text-white'>SoundBin</h1>
+    </a>
     </>
   )
 }
