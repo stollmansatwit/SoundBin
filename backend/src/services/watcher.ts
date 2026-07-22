d62685e5c6e988f043ca88c61aabc74c6f267826
IanDMacDougall <idmacdougall03@gmail.com>
2026-07-22 17:32:14 -0400

    updated watcher.ts

--- backend/src/services/watcher.ts
+++ backend/src/services/watcher.ts
@@ -28,9 +28,7 @@ const watcher = chokidar.watch(uploadPath, {
 
 
 // Helper functions
-function UnknownArtistAndAlbum() {
 
-}
 
 function getDate(dateString: string | undefined): Date | null {
 
@@ -65,9 +63,167 @@ function getDate(dateString: string | undefined): Date | null {
   return null
 }
 
-
 console.log(`Watching for new files in: ${uploadPath}`);
 
+
+/** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  Prisma Functions
+ */
+
+
+/**
+ * Finders
+ */
+async function findFirstGenre(genreName: string){
+  return (
+    await prisma.genre.findFirst({
+      where: {
+        name: {
+          equals: genreName,
+          mode: 'insensitive'
+        }
+      }
+    })
+  );
+}
+
+async function findFirstArtist(artistName: string){
+  return(
+    await prisma.artist.findFirst({
+      where: {
+        name: {
+          equals: artistName,
+          mode: 'insensitive'
+        }
+      }
+    })
+  );
+}
+
+async function findFistAlbum(albumTitle: string, artistID: number){
+  return (
+    await prisma.album.findFirst({
+      where: {
+        title: {
+          equals: albumTitle,
+          mode: `insensitive`
+        },
+      artist_id: artistID
+      }
+    })
+  );
+}
+
+async function findFirstTrack(title: string, duration: number){
+  return (
+    await prisma.track.findFirst({
+        where: {
+          title: {
+          equals: title,
+          mode: 'insensitive'
+        },
+        duration: duration
+      }
+    })
+  );
+}
+
+/**
+ * Creators
+ */
+async function createGenre(genreName: string){
+  return (
+    await prisma.genre.create({
+      data: { name: genreName }
+    })
+  );
+}
+
+async function createArtist(artistName: string){
+  return (
+    await prisma.artist.create({
+      data: {
+        name: artistName,
+        // Update for BIO & IMAGE_URL
+      }
+    })
+  );
+}
+
+async function createAlbum(albumTitle: string, artistID: number, date: any, coverUrl: string | null){
+  return(
+    await prisma.album.create({
+      data: {
+        title: albumTitle,
+        artist_id: artistID,
+        release_date: getDate(date),
+        cover_art_url: coverUrl,
+      }
+    })
+  );
+}
+
+async function createTrack(trackTitle: string, date: any, duration: number, coverUrl: string | null, albumID: number | null, genreID: number){
+  return (
+    await prisma.track.create({
+      data: {
+        title: trackTitle,
+        release_date: getDate(date),
+        duration: duration,
+        cover_art_url: coverUrl,
+        album: albumID ? { connect: { album_id: albumID } } : undefined,
+        genres: genreID ? { create: { genre_id: genreID } } : undefined,
+      }
+    })
+  );
+}
+
+async function createTrackSequence (albumID: number, trackID: number, trackNum: number){
+  return (
+    await prisma.albumTrackSequence.create({
+      data: {
+        album_id: albumID,
+        track_id: trackID,
+        sequence_number: trackNum
+      }
+    })
+  );
+}
+
+async function createTrackContributors(trackID: number, artistID: number, role: string ="artist"){
+  return (
+    await prisma.trackContributor.create({
+      data: {
+        track_id: trackID,
+        artist_id: artistID,
+        role: role,
+      }
+    })
+  );
+}
+
+async function createTrackFile(trackID: number, filePath: string, bitrate: number, sampleRate: number, channels: number, codec: string){
+  return (
+    await prisma.trackFile.create({
+      data: {
+        track_id: trackID,
+        storage_path_url: filePath,
+        bitrate: bitrate, // KBps
+        sample_rate: sampleRate,
+        channels: channels,
+        codec: codec,
+        file_mtime: new Date()
+        //file_hash String  @db.VarChar(64)
+      }
+    })
+  );
+}
+
+
+
+/**
+ * What watcher does when new file is found
+ */
 watcher.on('add', (filePath: string) => {
   console.log(`New file detected: ${filePath}`);
 
@@ -95,21 +251,12 @@ watcher.on('add', (filePath: string) => {
         const rawGenreName = Array.isArray(metadata.genre) ? metadata.genre[0] : metadata.genre;
         const genreName = rawGenreName ? String(rawGenreName).trim() || 'Unknown' : 'Unknown';
 
-        const existingGenre = await prisma.genre.findFirst({
-          where: {
-            name: {
-              equals: genreName,
-              mode: 'insensitive'
-            }
-          }
-        });
+        const existingGenre = await findFirstGenre(genreName);
 
         if (existingGenre) {
           genreID = existingGenre.genre_id;
         } else {
-          const newGenre = await prisma.genre.create({
-            data: { name: genreName }
-          });
+          const newGenre = await createGenre(genreName)
           genreID = newGenre.genre_id;
         }
 
@@ -118,104 +265,56 @@ watcher.on('add', (filePath: string) => {
         let artistID = null;
         //2.1. if metadata for artist exists, check if artist already exists
         if (metadata.artist && metadata.artist !== "Unknown Artist") {
-          const existingArtist = await prisma.artist.findFirst({
-            where: {
-              name: {
-                equals: metadata.artist,
-                mode: 'insensitive'
-              }
-            }
-          });
+          const existingArtist = await findFirstArtist(metadata.artist);
           // 2.2. If Artist exists, check for album under artist where = title
           if (existingArtist) {
             artistID = existingArtist.artist_id;
           } else { // Arist does not exist
-            const newArtist = await prisma.artist.create({
-              data: {
-                name: metadata.artist,
-                // Update for BIO & IMAGE_URL
-              }
-            });
+            const newArtist = await createArtist(metadata.artist);
             artistID = newArtist.artist_id;
           }
           // 2.3. if metdata for artist & album exists, check if album already exists
           if (metadata.album && metadata.album !== "Unknown Album") {
-            const existingAlbum = await prisma.album.findFirst({
-              where: {
-                title: {
-                  equals: metadata.album,
-                  mode: `insensitive`
-                },
-                artist_id: artistID
-              }
-            });
+            const existingAlbum = await findFistAlbum(metadata.album, artistID);
             // 2.4. If Album exists, get Album ID
             if (existingAlbum) {
               albumID = existingAlbum.album_id;
             } else { // Album doesn't exist
-
-
-              const newAlbum = await prisma.album.create({
-                data: {
-                  title: metadata.album,
-                  artist_id: artistID,
-                  release_date: getDate(metadata.date),
-                  cover_art_url: metadata.cover_url,
-                }
-
-              });
+              const newAlbum = await createAlbum(metadata.album, artistID, metadata.date, metadata.cover_url);
               albumID = newAlbum.album_id;
             }
             // 2.4. If album unknown add to known artist singles playlist
           } else if (metadata.album == "Unknown Album") {
             // upload to unknown
+            const existingUnknownAlbum = await findFistAlbum("Unknown Album", artistID);
+            // 2.8. If album exists add to singles "album"
+            if (existingUnknownAlbum) {
+              albumID = existingUnknownAlbum.album_id;
+            } else {
+              const newUnknownAlbum = await createAlbum("Unknown Album", artistID, metadata.date, null);
+              albumID = newUnknownAlbum.album_id;
+            }
 
           } else { console.log("ERROR FINDING METADATA OF ALBUM FROM FILE"); } // Album metadata not given, log error
         } else if (metadata.artist == "Unknown Artist") {
           // 2.5. If artisit unknown and album unknown, add to unknown 'album' / 'playlist'
-          const existingUnknownArtist = await prisma.artist.findFirst({
-            where: {
-              name: {
-                equals: 'Unknown',
-                mode: 'insensitive'
-              }
-            }
-          });
+          const existingUnknownArtist = await findFirstArtist("Unknown Artist");
           // 2.6. If Unknown Artist exists, check for album under artist where = title
           if (existingUnknownArtist) {
             artistID = existingUnknownArtist.artist_id;
           } else { // Arist does not exist
-            const newUnknownArtist = await prisma.artist.create({
-              data: {
-                name: metadata.artist,
-                // Update for BIO & IMAGE_URL
-              }
-            });
+            const newUnknownArtist = await createArtist(metadata.artist);
             artistID = newUnknownArtist.artist_id;
           }
           // 2.7. If album Unknown, album not in errors
           if (metadata.album == "Unknown Album") {
-            const existingUnkownAlbum = await prisma.album.findFirst({
-              where: {
-                title: {
-                  equals: `Unknown`,
-                  mode: `insensitive`
-                },
-              }
-            });
+            const existingUnknownAlbum = await findFistAlbum(metadata.album, artistID);
             // 2.8. If album exists add to singles "album"
-            if (existingUnkownAlbum) {
-              albumID = existingUnkownAlbum.album_id;
+            if (existingUnknownAlbum) {
+              albumID = existingUnknownAlbum.album_id;
             } else {
-              const newUnknownAlbum = await prisma.album.create({
-                data: {
-                  title: 'Unknown',
-                  artist_id: artistID,
-                  release_date: getDate(metadata.date),
-                  cover_art_url: null,
-                }
-              });
-              albumID = newUnknownAlbum.album_id;
+              const existingUnknownAlbum = await createAlbum("Unknown Album", artistID, metadata.date, null);
+              albumID = existingUnknownAlbum.album_id;
             }
           } else { console.log("ERROR FINDING METADATA OF ALBUM FROM FILE"); } // Album metadata not given, log error
         } else { console.log("ERROR FINDING METADATA OF ARTIST FROM FILE"); } // Artist metadata not given, log error
@@ -223,60 +322,27 @@ watcher.on('add', (filePath: string) => {
 
         // 3. Create track
         // should check if track already exists
-        const existingTrack = await prisma.track.findFirst({
-          where: {
-            title: {
-              equals: metadata.title,
-              mode: 'insensitive'
-            },
-            duration: metadata.duration,
-            
-          }
-        });
+        const existingTrack = await findFirstTrack(metadata.title, metadata.duration)
+
         var track;
         if (existingTrack) {
           console.log(`Track already exists: ${existingTrack.title}`);
           track = existingTrack;
         }
         else {
-          track = await prisma.track.create({
-            data: {
-              title: metadata.title,
-              release_date: getDate(metadata.date),
-              duration: metadata.duration,
-              cover_art_url: metadata.cover_url,
-              album: albumID ? { connect: { album_id: albumID } } : undefined,
-              genres: genreID ? { create: { genre_id: genreID } } : undefined,
-            }
-          });
+          track = await createTrack(metadata.title, metadata.date, metadata.duration, metadata.cover_url, albumID, genreID);
 
           // 4. Create Album Track Sequence
           const trackNum = metadata.track ? metadata.track : null;
           if (albumID && trackNum) {
-            await prisma.albumTrackSequence.create({
-              data: {
-                album_id: albumID,
-                track_id: track.track_id,
-                sequence_number: trackNum
-              }
-            });
+            createTrackSequence (albumID, track.track_id, trackNum);
           }
 
-          // 5. Handle Track Contributors ?
+          // 5. Handle Track Contributors
+          if (artistID !== null) createTrackContributors(track.track_id, artistID);
 
           // 6. Create Track File
-          await prisma.trackFile.create({
-            data: {
-              track_id: track.track_id,
-              storage_path_url: filePath,
-              bitrate: metadata.bitrate, // KBps
-              sample_rate: metadata.sample_rate,
-              channels: metadata.channels,
-              codec: metadata.codec,
-              file_mtime: new Date()
-              //file_hash String  @db.VarChar(64)
-            }
-          });
+          createTrackFile(track.track_id, filePath, metadata.bitrate, metadata.sample_rate, metadata.channels, metadata.codec);
           console.log(`Successfully indexed: ${track.title}`);
         }
       } catch (err) {
